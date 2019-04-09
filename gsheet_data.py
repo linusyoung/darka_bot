2bc7fba776fe18eb2af1b319926d0a9d5a080b15
linusyoung <linusyoungrice@gmail.com>
2019-04-09 20:12:18 +1000

    store last seen id in gsheet close #1

---
+++ gsheet_data.py
@@ -0,0 +1,33 @@
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from api_key import Keys
+import datetime
+
+
+class GSheet():
+
+    LAST_SEEN_RANGE = 'history!A2'
+
+    def __init__(self):
+        scope = ['https://www.googleapis.com/auth/spreadsheets',
+                 'https://www.googleapis.com/auth/drive']
+
+        creds = service_account.Credentials.from_service_account_file(
+            'client_secret.json', scopes=scope)
+        self.sheet = build('sheets', 'v4', credentials=creds).spreadsheets()
+
+    def get_last_seen_id(self):
+        request = self.sheet.values().get(spreadsheetId=Keys.SPREADSHEET_ID,
+                                          range=self.LAST_SEEN_RANGE)
+        response = request.execute()
+        return int(response.get('values')[0][0])
+
+    def update_last_seen_id(self, id):
+        value_input_option = 'RAW'
+        value_range_body = {
+            "range": self.LAST_SEEN_RANGE,
+            "values": [[str(id)]]
+        }
+        request = self.sheet.values().update(
+            spreadsheetId=Keys.SPREADSHEET_ID, range=self.LAST_SEEN_RANGE, valueInputOption=value_input_option, body=value_range_body)
+        request.execute()
--- main.py
+++ main.py
@@ -1,47 +1,24 @@
-# import tweepy
-# from api_key import Keys
+import tweepy
+from api_key import Keys
 
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
+from gsheet_data import GSheet
+# from pprint import pprint
 
+wks = GSheet()
+last_seen_id = wks.get_last_seen_id()
+
+print(last_seen_id)
 
 # get data from twitter
-# auth = tweepy.OAuthHandler(Keys.CONSUMER_KEY, Keys.CONSUMER_SECRET)
-# auth.set_access_token(Keys.ACCESS_KEY, Keys.ACCESS_SECRET)
+auth = tweepy.OAuthHandler(Keys.CONSUMER_KEY, Keys.CONSUMER_SECRET)
+auth.set_access_token(Keys.ACCESS_KEY, Keys.ACCESS_SECRET)
 
-# api = tweepy.API(auth)
+api = tweepy.API(auth)
 
-# mentions = api.mentions_timeline()
+mentions = api.mentions_timeline(since_id=last_seen_id)
 
-# # for mention in mentions:
-
-
-# test_mention = mentions[0]
-
-# api.update_status('@' + test_mention.user.screen_name +
-#                   ' Roger that!', test_mention.id)
-
-# print(str(test_mention.id) + ': ' + test_mention.text)
-
-spreadsheet_id = '1uwjJCXNgXBzf_IaBj1hwl_y5pJ54XW5FKtKecrgHM58'
-data_range = 'logs!A1:B2'
-
-# interactive with gspread
-scope = ['https://www.googleapis.com/auth/spreadsheets',
-         'https://www.googleapis.com/auth/drive']
-
-creds = service_account.Credentials.from_service_account_file(
-    'client_secret.json', scopes=scope)
-
-service = build('sheets', 'v4', credentials=creds)
-
-sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=spreadsheet_id, range=data_range).execute()
-
-values = result.get('values', [])
-
-if not values:
-    print('No data found.')
-else:
-    for row in values:
-        print(row)
+for mention in reversed(mentions):
+    print(str(mention.id) + ': ' + mention.text)
+    wks.update_last_seen_id(mention.id)
+    # api.update_status('@' + test_mention.user.screen_name +
+    #                   ' Roger that!', test_mention.id)
